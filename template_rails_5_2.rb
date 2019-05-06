a2770c2e30c35f3e5900a7b7c54403a2ce166dfd
Hidenori Miyoshi <miyohide@gmail.com>
2019-05-06 18:22:46 +0900

    Add template for Rails 5.2.
    remove comments from Gemfile.

---
+++ template_rails_5_2.rb
@@ -0,0 +1,25 @@
+require "fileutils"
+
+# pathで指定されたファイルに対してblockの処理を実行して
+# 戻り値が偽のものを集めてファイルに書き込む。
+# 条件にマッチしなかったものだけを集める=不要なものを削除する
+# という使い方をする。
+def filter_lines(path, &block)
+  files = Dir.glob(path).select { |f| File.file?(f) }
+  files.each do |f|
+    content = File.read(f)
+    filtered = content.lines.reject do |l|
+      block.call(l)
+    end.join
+    File.write(f, filtered)
+  end
+end
+
+# コメント行を削除する
+def remove_comments(path)
+  filter_lines(path) do |l|
+    l.strip.start_with?("#")
+  end
+end
+
+remove_comments("Gemfile")
